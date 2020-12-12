c9ae30dd3d4487c2f64f32c4d4a514a3fe10c24d
Ivaylo <ivaylo.georgiev18@gmail.com>
2020-12-13 00:23:19 +0200

    Create FengShuiDetails.java

---
+++ src/main/java/com/fmi/fengshuipdfgenerator/FengShuiDetails.java
@@ -0,0 +1,87 @@
+package com.fmi.fengshuipdfgenerator;
+
+public class FengShuiDetails {
+
+	private String chineseYearSign;
+	private String chineseHourSign;
+	private String astrologyAllies;
+	private String secretFriend;
+	private String astrologyEnemy;
+	private String peachBlossomAnimal;
+	private int kuaNumber;
+	private String fourBestDirections;
+	private String fourWorstDirections;
+
+	public String getChineseYearSign() {
+		return chineseYearSign;
+	}
+
+	public void setChineseYearSign(String chineseYearSign) {
+		this.chineseYearSign = chineseYearSign;
+	}
+
+	public String getChineseHourSign() {
+		return chineseHourSign;
+	}
+
+	public void setChineseHourSign(String chineseHourSign) {
+		this.chineseHourSign = chineseHourSign;
+	}
+
+	public String getAstrologyAllies() {
+		return astrologyAllies;
+	}
+
+	public void setAstrologyAllies(String astrologyAllies) {
+		this.astrologyAllies = astrologyAllies;
+	}
+
+	public String getSecretFriend() {
+		return secretFriend;
+	}
+
+	public void setSecretFriend(String secretFriend) {
+		this.secretFriend = secretFriend;
+	}
+
+	public String getAstrologyEnemy() {
+		return astrologyEnemy;
+	}
+
+	public void setAstrologyEnemy(String astrologyEnemy) {
+		this.astrologyEnemy = astrologyEnemy;
+	}
+
+	public String getPeachBlossomAnimal() {
+		return peachBlossomAnimal;
+	}
+
+	public void setPeachBlossomAnimal(String peachBlossomAnimal) {
+		this.peachBlossomAnimal = peachBlossomAnimal;
+	}
+
+	public int getKuaNumber() {
+		return kuaNumber;
+	}
+
+	public void setKuaNumber(int kuaNumber) {
+		this.kuaNumber = kuaNumber;
+	}
+
+	public String getFourBestDirections() {
+		return fourBestDirections;
+	}
+
+	public void setFourBestDirections(String fourBestDirections) {
+		this.fourBestDirections = fourBestDirections;
+	}
+
+	public String getFourWorstDirections() {
+		return fourWorstDirections;
+	}
+
+	public void setFourWorstDirections(String fourWorstDirections) {
+		this.fourWorstDirections = fourWorstDirections;
+	}
+
+}
